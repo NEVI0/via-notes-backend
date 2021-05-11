674867cfcda0ff901c58064d147db641f6893618
NEVI0 <neviocostamagagnin@gmail.com>
2021-05-11 09:20:48 -0300

    Adicionado os handlers de insert, update e delete note

--- src/controllers/noteController.ts
+++ src/controllers/noteController.ts
@@ -25,7 +25,16 @@ export default class NoteController {
 	public async insert(request: Request, response: Response) {
 		try {
 
-			
+			const { id_user, description } = request.body;
+
+			if (!description) {
+				return response.status(401).json({ message: 'Informe a descrição da sua nota!' });
+			}
+
+			const noteObj: Note = new Note();
+			await noteObj.insert(id_user, description);
+
+			return response.status(200).json({ message: 'Sua nota foi criada com sucesso!' });
 
 		} catch (err) {
 			return response.status(500).json({ message: 'Não foi possível criar a sua nota!', err });
@@ -35,7 +44,17 @@ export default class NoteController {
 	public async update(request: Request, response: Response) {
 		try {
 
-			
+			const { id_note } = request.params;
+			const { id_status, description } = request.body;
+
+			if (!id_status || !description) {
+				return response.status(401).json({ message: 'Informe o status e a descrição da sua nota!' });
+			}
+
+			const noteObj: Note = new Note();
+			await noteObj.update(id_note, id_status, description);
+
+			return response.status(200).json({ message: 'Sua nota foi atualizada com sucesso!' });
 
 		} catch (err) {
 			return response.status(500).json({ message: 'Não foi possível atualizar a sua nota!', err });
@@ -45,7 +64,16 @@ export default class NoteController {
 	public async delete(request: Request, response: Response) {
 		try {
 
+			const { id_note } = request.params;
 			
+			if (!id_note) {
+				return response.status(401).json({ message: 'O ID da nota não foi informado!' });
+			}
+
+			const noteObj: Note = new Note();
+			await noteObj.delete(id_note);
+
+			return response.status(200).json({ message: 'Sua nota foi deletada!' });
 
 		} catch (err) {
 			return response.status(500).json({ message: 'Não foi possível deletar a sua nota!', err });
