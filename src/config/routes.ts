820496e684e8d2f2be14501de409aeedb6880dc3
NEVI0 <neviocostamagagnin@gmail.com>
2021-05-14 11:22:39 -0300

    Adicionado as rotas aberta e bloqueado as rotas da api

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,15 +1,28 @@
 import express from 'express';
 
+import UserController from '../controllers/userController';
 import NoteController from '../controllers/noteController';
 import StatusController from '../controllers/statusController';
+
+import auth from './auth';
 
 const routes = (server: express.Application) => {
 	try {
 
+		const openApi = express.Router();
+		server.use('/open-api', openApi);
+		
+		openApi.get('/', (request, response) => {
+			return response.status(200).json({ message: 'Via Notes API is running!' })
+		});
+		
+		openApi.post('/signin', UserController.prototype.signin);
+		openApi.post('/signup', UserController.prototype.signup);
+		openApi.post('/validate', UserController.prototype.validateToken);
+
 		const api = express.Router();
 		server.use('/api', api);
-
-		api.get('/', (request, response) => response.status(200).json({ message: 'Via Notes API is running!' }));
+		api.use(auth);
 
 		api.get('/note/:id_user', NoteController.prototype.get);
 		api.post('/note', NoteController.prototype.insert);
@@ -17,6 +30,7 @@ const routes = (server: express.Application) => {
 		api.delete('/note/:id_note', NoteController.prototype.delete);
 
 		api.get('/status', StatusController.prototype.get);
+		api.delete('/user/:id_user', UserController.prototype.delete);
 		
 	} catch (err) {
 		console.log('Routes error: ', err);
